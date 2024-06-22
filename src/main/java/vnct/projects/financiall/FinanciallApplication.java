a52092c1f7167c2f8800ad0db923fe1df1770770
vincentruijgrok <vincentjohanruijgrok@gmail.com>
2024-06-22 16:34:31 +0200

    project setup

---
+++ src/main/java/vnct/projects/financiall/FinanciallApplication.java
@@ -0,0 +1,21 @@
+package vnct.projects.financiall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application class for Financiall.
+ */
+@SpringBootApplication
+public class FinanciallApplication {
+
+	/**
+	* Run the SpringApplication.
+
+	* @param args Application arguments
+	*/
+	public static void main(String[] args) {
+		SpringApplication.run(FinanciallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vnct/projects/financiall/FinanciallApplicationTests.java
@@ -0,0 +1,20 @@
+package vnct.projects.financiall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanciallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void runServer() {
+
+		FinanciallApplication.main(new String[]{});
+
+	}
+
+}
